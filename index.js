12df2a75a09ee430a4d1fed684b6d48ad071cb93
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-08-23 08:26:29 -0600

    name

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(change) {
+    bestCustomer = change;
+}
+const leastFavoriteCustomer = "Elon Musk";
+function changeLeastFavoriteCustomer(change) {
+    leastFavoriteCustomer = change;
+}
